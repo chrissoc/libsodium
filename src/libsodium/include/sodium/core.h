375fff91912b9304d791db2487f9b56fd5c44a70
Christopher Carlson <christopher.carlson.a@gmail.com>
2018-02-18 16:28:25 -0800

    Fixed no return warning for VS2015

--- src/libsodium/include/sodium/core.h
+++ src/libsodium/include/sodium/core.h
@@ -8,6 +8,14 @@
 extern "C" {
 #endif
 
+#ifdef _MSC_VER
+#define SODIUM_DECL_NORETURN __declspec(noreturn)
+#define SODIUM_ATTR_NORETURN
+#else
+#define SODIUM_DECL_NORETURN
+#define SODIUM_ATTR_NORETURN __attribute__ ((noreturn))
+#endif
+
 SODIUM_EXPORT
 int sodium_init(void)
             __attribute__ ((warn_unused_result));
@@ -18,8 +26,8 @@ SODIUM_EXPORT
 int sodium_set_misuse_handler(void (*handler)(void));
 
 SODIUM_EXPORT
-void sodium_misuse(void)
-            __attribute__ ((noreturn));
+void SODIUM_DECL_NORETURN sodium_misuse(void)
+            SODIUM_ATTR_NORETURN;
 
 #ifdef __cplusplus
 }
